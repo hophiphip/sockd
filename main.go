ac264170ad55d63e20f4f86682327d0351a7fc1e
hophiphip <nighop@yandex.ru>
2021-08-12 16:56:42 +0400

    Pipes instead of streams

--- main.go
+++ main.go
@@ -1,25 +1,59 @@
 package main
 
 import (
-	"io"
+	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"time"
 )
 
 func main() {
-	cmd := exec.Command("ls", "-la")
+	//cmd := exec.Command("/bin/sh", "-c", "while true; do echo 'loop'; sleep 5; done")
+	cmd := exec.Command("cat")
+
+	cmd.Stderr = os.Stderr
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer stdout.Close()
 
-	go io.Copy(os.Stdout, stdout)
+	stdoutScanner := bufio.NewScanner(stdout)
 
 	if err = cmd.Start(); err != nil {
 		log.Fatal(err)
 	}
+
+	go func() {
+		for stdoutScanner.Scan() {
+			fmt.Printf("[%s] >> %s\n", time.Now().Format(time.RFC850), stdoutScanner.Text())
+		}
+	}()
+
+	go func() {
+		consoleReader := bufio.NewReader(os.Stdin)
+
+		// Indefinetly read from os.Stdin
+		// NOTE: it is probably impossible to tell when cmd.Stdin requires input
+		for {
+			fmt.Printf("[%s] << ", time.Now().Format(time.RFC850))
+			input, err := consoleReader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Print("\n")
+
+			stdin.Write([]byte(input))
+		}
+	}()
 
 	if err = cmd.Wait(); err != nil {
 		log.Fatal(err)
